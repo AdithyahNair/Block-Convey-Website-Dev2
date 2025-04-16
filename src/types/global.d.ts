29a56bf8d126e4674a3df24bbc13a5c75d3b7083
Adithyah Nair <an4465@nyu.edu>
2025-04-16 18:42:03 -0400

    GA fix 3

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,4 @@
-interface Window {
-  dataLayer: unknown[];
-  gtag: (...args: unknown[]) => void;
-}
+// interface Window {
+//   dataLayer: unknown[];
+//   gtag: (...args: unknown[]) => void;
+// }
