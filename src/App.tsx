bb19e181242fd441365664ff9701207747547e8e
Adithyah Nair <an4465@nyu.edu>
2025-04-16 17:58:45 -0400

    Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
+import { useEffect } from "react";
 import { LandingPage } from "./pages/LandingPage";
 import { FeaturesPage } from "./pages/FeaturesPage";
 import { PricingPage } from "./pages/PricingPage";
@@ -14,9 +21,49 @@ import { BlogListPage } from "./pages/BlogListPage";
 import { BlogDetailPage } from "./pages/BlogDetailPage";
 // import { ComingSoonPage } from "./pages/ComingSoonPage";
 
+// Google Analytics tracking (this is just the initialization)
+function initializeGoogleAnalytics() {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${
+    import.meta.env.VITE_GA_MEASUREMENT_ID
+  }`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function (...args) {
+    window.dataLayer.push(args);
+  };
+  window.gtag("js", new Date());
+  window.gtag("config", import.meta.env.VITE_GA_MEASUREMENT_ID);
+}
+
+// Analytics tracking component
+function AnalyticsTracker() {
+  const location = useLocation();
+  const navigationType = useNavigationType();
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("event", "page_view", {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location, navigationType]);
+
+  return null;
+}
+
 function App() {
+  useEffect(() => {
+    initializeGoogleAnalytics();
+  }, []);
+
   return (
     <Router>
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/solutions" element={<FeaturesPage />} />
