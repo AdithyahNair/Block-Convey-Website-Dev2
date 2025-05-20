3958131eaeff279e28bcfb7bf85cd87bcd11e0c4
Adithyah Nair <adithyah.nair@gmail.com>
2025-05-20 10:26:51 -0400

    updated video

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -177,7 +177,7 @@ export const Hero: React.FC = () => {
               allow="fullscreen;autoplay"
               allowFullScreen
               height="100%"
-              src="https://streamable.com/e/room9d"
+              src="https://streamable.com/qgehol"
               width="100%"
               style={{
                 border: "none",
