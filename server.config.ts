40c84699670fdc41bd2bebbcf5fa84b273e54512
Adithyah Nair <adithyah.nair@gmail.com>
2025-06-24 14:46:54 -0400

    Fix server configuration for App Engine deployment

--- server.config.ts
+++ server.config.ts
@@ -5,9 +5,14 @@ export default defineConfig({
   build: {
     ssr: resolve(__dirname, "server.ts"),
     outDir: "dist/server",
-    target: "node18",
+    target: "node20",
     minify: false,
     rollupOptions: {
+      input: "server.ts",
+      output: {
+        format: "esm",
+        entryFileNames: "server.js",
+      },
       external: [
         "express",
         "firebase/app",
