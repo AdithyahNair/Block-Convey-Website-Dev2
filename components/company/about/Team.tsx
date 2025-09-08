9dd7d285279d1a6d8b701531d86f9b91e79a5af1
Adithyah Nair <adithyah.nair@gmail.com>
2025-09-08 12:58:27 -0400

    refactoring

--- components/company/about/Team.tsx
+++ components/company/about/Team.tsx
@@ -150,7 +150,7 @@ export const Team: React.FC = () => {
           >
             Advisory Board
           </motion.h3>
-          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-4xl mx-auto">
             {advisors.map((advisor, index) => (
               <motion.div
                 key={index}
